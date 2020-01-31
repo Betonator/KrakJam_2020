40979f7915bd6e366b34b2d6aa175996fd6ce04f
Unknown <p.przebieglec@gmail.com>
2020-01-31 21:10:11 +0100

    WIP

---
+++ Assets/Scripts/Morda.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class Morda : MonoBehaviour
+{
+    private bool isPatykPickable = false;
+    private Rigidbody pickablePatyk = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("PickUp")) {
+            PickUpPatyk();
+        }
+    }
+
+    private void PickUpPatyk() {
+        if(isPatykPickable) {
+            Debug.Log("dupa");
+            Debug.Log(pickablePatyk);
+            pickablePatyk.transform.position += Vector3.up * 1.0f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Patyk") {
+            Debug.Log(collision.collider.name);
+            isPatykPickable = true;
+            pickablePatyk = collision.rigidbody;
+        }
+    }
+}
