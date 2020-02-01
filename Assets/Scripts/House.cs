39138dc175d79537ff8d627809b227143d37fbb5
Kacper <kap3r3k@gmail.com>
2020-02-01 10:42:32 +0100

    HouseScriptStarted

---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    public int houseIndex = 1;
+    private int startingLevel = 0;
+    public int maxLevel = 4;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Stick"))
+        {
+
+        }
+    }
+}
