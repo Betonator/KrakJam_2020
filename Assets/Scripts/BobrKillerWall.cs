a1a5ae38deb501d1a64555f01f1bb7eb7c7d9a4b
Kacper <kap3r3k@gmail.com>
2020-02-02 02:03:05 +0100

    MapSetupV2 + StickBalance, chce się bawić do rana

---
+++ Assets/Scripts/BobrKillerWall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobrKillerWall : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BOBR"))
+        {
+            other.GetComponent<BOBR_Move>().TakeDMG(1000000f);
+        }
+    }
+}
